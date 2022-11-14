0400b0f11010d5fc244bfd6fb1e9b392385bcbde
shin <vanvietkhtn@gmail.com>
2022-11-15 01:14:46 +0700

    Finish API development

--- api/app.py
+++ api/app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, jsonify
 from api.config import config
 from api.models import db
-from api.blueprints import todos
+from api.blueprints import todos, generate_response
 
 
 app = Flask(__name__)
@@ -20,6 +20,16 @@ with app.app_context():
 @app.route('/api')
 def hello():
     return jsonify({'response': "It's working"})
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return generate_response(404, 'Resource not found.')
+
+
+@app.errorhandler(400)
+def bad_request(e):
+    return generate_response(400, 'Bad request.')
 
 
 app.register_blueprint(todos)
--- api/blueprints.py
+++ api/blueprints.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 from api.models import Todo, db
-from api.utils import todo_to_dict
+from api.utils import todo_to_dict, generate_response
 
 
 todos = Blueprint('todos', __name__)
@@ -14,6 +14,8 @@ def list_all_todos():
 @todos.route('/todos/<int:todo_id>', methods=['GET'])
 def list_todo(todo_id):
     todo = Todo.query.filter_by(id=todo_id).first()
+    if not todo:
+        return generate_response(404, 'Task not found.')
     return jsonify(todo_to_dict(todo))
 
 
@@ -22,23 +24,37 @@ def add_todo():
     post_data = request.get_json()
     print(post_data)
     if not post_data:
-        response = {'code': 400, 'message': 'Invalid payload.'}
-        return jsonify(response), response.get('code')
+        return generate_response(400, 'Invalid payload.')
 
     task = post_data.get('task')
     todo = Todo(task=task)
     db.session.add(todo)
     db.session.commit()
-
-    response = {'code': 201, 'message': 'Task added.'}
-    return jsonify(response), response.get('code')
+    return generate_response(201, 'Task added.')
 
 
-@todos.route('/todos/<int:todo_id>', methods=['PATCH'])
-def update_todo():
-    pass
+@todos.route('/todos/<int:todo_id>', methods=['PUT'])
+def update_todo(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    if not todo:
+        return generate_response(404, 'Task not found.')
+
+    post_data = request.get_json()
+    if not post_data:
+        return generate_response(400, 'Invalid payload.')
+    todo.task = post_data.get('task')
+    todo.done = post_data.get('done')
+    db.session.add(todo)
+    db.session.commit()
+    return generate_response(200, 'Task updated.')
 
 
 @todos.route('/todos/<int:todo_id>', methods=['DELETE'])
-def delete_todo():
-    pass
+def delete_todo(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    if not todo:
+        return generate_response(404, 'Task not found.')
+
+    db.session.delete(todo)
+    db.session.commit()
+    return generate_response(200, 'Task deleted.')
